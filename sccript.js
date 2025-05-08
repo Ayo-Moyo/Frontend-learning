ada28b25c49f304814ea7f97a666810721d3845d
Ayokunnumi Oyesile <ayokunnumi.oyesile@gmail.com>
2025-05-08 14:50:42 +0100

    assignments

---
+++ sccript.js
@@ -0,0 +1,30 @@
+//  1. mathematical operators
+let x= 20;
+let y=10;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+
+// 2. comparison operators
+let p=5;
+let q=1;
+let str1= "Mary"
+let str2= "Anne"
+
+console.log(p==q);
+console.log(p!=q);
+console.log(p>q);
+console.log(p<q);
+console.log(p>=q);
+console.log(p<=q);
+
+console.log(str1==str2);
+console.log(str1!=str2);
+
+// 3. Logical operators
+console.log(11>2&&14<20);
+console.log(11<2||14<20);
+console.log(!(13<20));
