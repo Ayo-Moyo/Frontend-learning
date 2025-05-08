a2b579bedb288f2c6d200bb96d327fdf8dced9d1
Ayokunnumi Oyesile <ayokunnumi.oyesile@gmail.com>
2025-05-08 15:36:48 +0100

    assignments

--- sscript.js
+++ sscript.js
@@ -1,4 +1,34 @@
+// 1.
 function greeting(){
     console.log("Hello Patricia")
 };
 greeting()
+
+// 2.
+function add(l,m){
+    const sum = l + m;
+    console.log (sum, "sum")
+    return sum;
+}
+add(3,4);
+
+function subtract(p,q){
+    const diff = p-q;
+    console.log(diff, "diff");
+    return diff;
+}
+subtract(15,4);
+
+function multiply(r,s){
+    const product = r*s;
+    console.log(product, "product");
+    return product;
+}
+multiply(24,78)
+
+function divide(c,d){
+    const quotient = c/d;
+    console.log(quotient, "quotient");
+    return quotient;
+}
+divide(202,34);
